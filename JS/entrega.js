d1e3ebcd5a3ccb42093ef9d7500250769624bcfa
Ezequiel Klausing <ezequielfl@gmail.com>
2022-08-17 19:35:11 -0300

    localstorage en pushbar

--- JS/entrega.js
+++ JS/entrega.js
@@ -1,52 +1,13 @@
-let divArticulos = document.getElementById("articulos");
-
-remeras.forEach((remera) => {
-  let nuevoArticulo = document.createElement("div");
-  nuevoArticulo.innerHTML = `<div id = "${remera.id}" class="card" style="width: 18rem;">
-  <img class="card-img-top" src="${remera.imagen}" alt="${remera.nombre}">
-  <div class="card-body">
-    <h5 class="card-title">Precio $ ${remera.precio}</h5>
-    <p class="card-text">${remera.nombre}</p>
-    <a href="#" id = "agregaBtn ${remera.id}" class="btn btn-primary">Agregar al carrito</a>
-  </div>
-  </div>
-  <br>
-  `;
-
-  divArticulos.appendChild(nuevoArticulo);
-  let botonAgregar = document.getElementById(`agregaBtn ${remera.id}`);
-  botonAgregar.addEventListener("click", () => {
-    agregarAlCarrito(remera);
-  });
-});
-
-function agregarAlCarrito(remera) {
-  alert(
-    `Se agrego al carrito articulo: ${remera.nombre}, Id producto nro ${remera.id} `
-  );
-  carrito.push(remera);
-  localStorage.setItem("carrito", JSON.stringify(carrito));
-  console.log(carrito);
+class producto {
+  constructor(id, nombre, precio, descuento, imagen, stock) {
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+    this.descuento = descuento;
+    this.imagen = imagen;
+    this.stock = stock;
+  }
 }
-/* Productos en el carrito - Pushbar */
-let divProductosEnCarrito = document.getElementById("productosEnCarrito");
-
-carrito.forEach((remera) => {
-  let nuevoArticulo = document.createElement("div");
-  nuevoArticulo.innerHTML = `<div class="producto">
-  <img src="./assets/remera negra.jpg" alt="" />
-  <div class="descripcion">
-  <p>Remera Negra</p>
-  <p>Precio 2000</p>
-  </div>
-</div>
-`;
-  divProductosEnCarrito.appendChild(nuevoArticulo);
-  // let botonAgregar = document.getElementById(`agregaBtn ${remera.id}`);
-  // botonAgregar.addEventListener("click", () => {
-  //   agregarAlCarrito(remera);
-  // });
-});
 
 /*Iniciar array Remeras en localstorage */
 
@@ -64,18 +25,66 @@ if (localStorage.getItem("carrito")) {
   localStorage.setItem("carrito", []);
 }
 
+/*Dom */
+
+let divArticulos = document.getElementById("articulos");
+let divProductosEnCarrito = document.getElementById("productosEnCarrito");
+
+/* Productos en stock */
+remeras.forEach((remera) => {
+  let nuevoArticulo = document.createElement("div");
+  nuevoArticulo.innerHTML = `<div id = "${remera.id}" class="card" style="width: 18rem;">
+  <img class="card-img-top" src="${remera.imagen}" alt="${remera.nombre}">
+  <div class="card-body">
+    <h5 class="card-title">Precio $ ${remera.precio}</h5>
+    <p class="card-text">${remera.nombre}</p>
+    <a href="#" id = "agregaBtn ${remera.id}" class="btn btn-primary">Agregar al carrito</a>
+  </div>
+  </div>
+  <br>
+  `;
+  divArticulos.appendChild(nuevoArticulo);
+  let botonAgregar = document.getElementById(`agregaBtn ${remera.id}`);
+  botonAgregar.addEventListener("click", () => {
+    agregarAlCarrito(remera);
+  });
+});
+
+function agregarAlCarrito(remera) {
+  alert(
+    `Se agrego al carrito articulo: ${remera.nombre}, Id producto nro ${remera.id} `
+  );
+  carrito.push(remera);
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+  console.log(carrito);
+  let nuevoArticulo = document.createElement("div");
+  nuevoArticulo.innerHTML = `<div class="producto">
+        <img src="${remera.imagen}" alt="${remera.nombre}" />
+        <div class="descripcion">
+        <p>${remera.nombre}</p>
+        <p>$ ${remera.precio}</p>
+        </div>
+      </div>
+      `;
+  divProductosEnCarrito.appendChild(nuevoArticulo);
+}
+
+/* Productos en el carrito - Pushbar */ /* ver de simpliflicar */
+
+carrito.forEach((remera) => {
+  let nuevoArticulo = document.createElement("div");
+  nuevoArticulo.innerHTML = `<div class="producto">
+        <img src="${remera.imagen}" alt="${remera.nombre}" />
+        <div class="descripcion">
+        <p>${remera.nombre}</p>
+        <p>$ ${remera.precio}</p>
+        </div>
+      </div>
+      `;
+  divProductosEnCarrito.appendChild(nuevoArticulo);
+});
+
 function totalCompra() {
   let total = carrito.reduce((total, remera) => total + remera.precio, 0);
   console.log(total);
-}
-
-class producto {
-  constructor(id, nombre, precio, descuento, imagen, stock) {
-    this.id = id;
-    this.nombre = nombre;
-    this.precio = precio;
-    this.descuento = descuento;
-    this.imagen = imagen;
-    this.stock = stock;
-  }
 }
